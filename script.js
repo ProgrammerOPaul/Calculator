11a064af91f20096f379716d91f0dd3f52a40f4e
Paul Ortiz <paulortiz10@outlook.com>
2023-01-31 12:38:09 -0400

    Add float functionality

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const numbers = document.getElementsByClassName('numbers');
 const operators = document.getElementsByClassName('operators');
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
+const equalsBtn = document.getElementById('equals');
 let num1 = null;
 let num2 = null;
+let op = '';
+let check = 0;
+let start = "no"
 
 function mathOperations(operator, num1, num2){
     switch(operator){
@@ -23,15 +27,18 @@ function mathOperations(operator, num1, num2){
 
 function operate(operator){
     if (num1 == null){
-        num1 = parseInt(display.value);
-        display.value = "";
+        num1 = parseFloat(display.value);
+        op = operator;
+        start = 'yes'
     }
     else{
-        num2 = parseInt(display.value);
-        display.value = mathOperations(operator, num1, num2);
+        num2 = parseFloat(display.value);
+        display.value = mathOperations(op, num1, num2).toFixed(2);
         num1 = null;
+        num2 = null;
+        op = '';
+        check = 0;
     }
-
 }
 
 function clear(){
@@ -48,20 +55,37 @@ function deleteSpc(){
 }
 
 function calculator(){
+
     for (let i = 0; i < numbers.length; i++){
         numbers[i].addEventListener('click', function(){
             if (display.value == 0){
                 display.value = "";
+            } else if (op != '' && check == 0){
+                display.value = "";
+                check = 1;
             };
-            display.value += this.value;
+            display.value += this.value;    
         })};
+
     for (let i = 0; i < operators.length; i++){
         operators[i].addEventListener('click', function(){
-            operate(this.value);
-        })};
+            if (start == "yes"){
+                console.log("im in")
+                operate(this.value);
+                num1 = parseFloat(display.value);
+                op = this.value;
+                check = 0;
+            } else  {
+                operate(this.value);
+            }
+    })};
 }
-
 
 calculator();
 clearBtn.addEventListener('click', clear);
 deleteBtn.addEventListener('click', deleteSpc)
+equalsBtn.addEventListener('click', function(){
+    if (num1 != null && op != ""){
+        operate(op);
+    }
+})
